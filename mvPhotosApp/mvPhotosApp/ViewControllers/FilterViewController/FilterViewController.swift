256c8b74c650d7df0b7aec5dc34b2c71d702f0fc
Mikhail Kirillov <kirillov@sibext.com>
2018-04-09 21:54:13 +0700

    [+] Filter view controller

---
+++ mvPhotosApp/mvPhotosApp/ViewControllers/FilterViewController/FilterViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FilterViewController.swift
+//  mvPhotosApp
+//
+//  Created by Mikhail Kirillov on 09/04/2018.
+//  Copyright © 2018 Mikhail Kirillov. All rights reserved.
+//
+
+import UIKit
+
+class FilterViewController: UIViewController {
+    
+}
