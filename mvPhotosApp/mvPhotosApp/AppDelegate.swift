0280b87ed8d7422dad969ada1a75f04f8236dbc1
Mikhail Kirillov <kirillov@sibext.com>
2018-04-09 21:19:01 +0700

    [*] Initial commit

---
+++ mvPhotosApp/mvPhotosApp/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  mvPhotosApp
+//
+//  Created by Mikhail Kirillov on 09/04/2018.
+//  Copyright © 2018 Mikhail Kirillov. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
